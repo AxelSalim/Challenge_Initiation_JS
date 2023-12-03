c1502808463bfaed8e36b5a0e56e986a9cef7e56
AxelSalim <adjakidjememiaghe@gmail.com>
2023-12-03 17:07:21 +0100

    utilisation DOM

--- js/script1.js
+++ js/script1.js
@@ -2,20 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     
-    let array = [], prenom, prenoms;
-
-    while (true) {
-
-        prenom = prompt('Entrez un prénom : ');
-
-        if (prenom) {
-            prenoms += prenom;
-            array.push(prenom);
-            alert(array);
-        }
-        else {
-            break;
-        }
-    }
+    
 
 });
